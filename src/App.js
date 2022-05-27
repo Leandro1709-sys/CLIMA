0a92bd1c6e8965a5cb22fe7012125f5984ca0b7f
Leandro1709-sys <martinezvalenzuela@gmail.com>
2022-05-26 21:48:13 -0300

    ultimo commit con https con css metrico

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [data,  setData] = React.useState([])
 
   function onSearch(ciudad) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ciudad+'&appid=4ae2636d8dfbdc3044bede63951a019b')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ciudad+'&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric')
       .then(r => r.json())
       .then((recurso) => {
         if(recurso?.cod !== '404'&&recurso!==''){
